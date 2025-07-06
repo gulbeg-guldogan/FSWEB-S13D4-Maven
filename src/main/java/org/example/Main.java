beab352d508bbc3dac47a647fa8a938125110b5d
gulbeg-guldogan <gulbeg.guldogan@gmail.com>
2025-07-06 20:56:16 +0300

    problem solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Player player = new Player("emrah", 100, Weapon.AXE);
+        System.out.println("healthremaining: " + player.healthRemaining());
+
+        Player player2 = new Player("hakan", 90, Weapon.SWORD);
+        System.out.println("healthremaining: " + player2.healthRemaining());
+
+        player.loseHealth((int) (player2.getWeapon().getDamage() * player2.getWeapon().getAttackSpeed()));
+        System.out.println("healthremaining after damage: " + player.healthRemaining());
+
+        player.restoreHealth(70);
+        System.out.println("healthremaining after restore: " + player.healthRemaining());
     }
 }
